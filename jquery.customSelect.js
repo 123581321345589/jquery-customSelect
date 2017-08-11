11acd712cb69b36775d67246364e6f9416f31ff4
123581321345589 <vasya.chernenkov@yandex.ru>
2017-08-11 11:51:53 +0300

    Update jquery.customSelect.js
    Исправил ошибки в работе

--- jquery.customSelect.js
+++ jquery.customSelect.js
@@ -1,221 +1,256 @@
-(function($) {
+/* Custom Select v1.0 | (c) real.Master | clearex.ru */
+;(function($) {
 
-  $.fn.customSelect = function(action) {
+	$.fn.customSelect = function(action) {
 
-    action = action || 'init';
+		action = action || 'init';
 
-    var lng = {
-      'no_options': 'Пусто',
-      'no_results': 'Ничего не найдено'
-    };
+		var doc = $(document);
 
-    if (action == 'init') {
-      return this.each(function(i, e) {
+		var lng = {
+			'no_options': 'Пусто',
+			'no_results': 'Ничего не найдено'
+		};
 
-        var
-        def_select  = $(e),
-        def_options = def_select.find('option'),
-        is_multiple = def_select.attr('multiple') ? true : false,
-        is_search   = def_select.hasClass('no_search') ? false : true;
-        is_options  = def_options.length ? true : false;
-        is_search   = is_search && is_options ? true : false;
+		if (action == 'init') {
+			return this.each(function(i, e) {
 
-        if (def_select.next('.select').length) return;
+				var
+				def_select  = $(e),
+				def_options = def_select.find('option'),
+				is_multiple = def_select.attr('multiple') ? true : false,
+				is_search   = def_select.hasClass('no_search') ? false : true;
+				is_options  = def_options.length ? true : false;
+				is_search   = is_search && is_options ? true : false;
 
-        // Скрыть дефолтный селект
-        def_select.hide();
+				if (def_select.next('.select').length) return;
 
-        // Блок поиска
-        var html_search = is_search ? '<div class="search"><input type="text" name="custom_select_search"></div>' : '';
+				// Скрыть дефолтный селект
+				def_select.hide();
 
-        // Блок поиска
-        var html_options = is_options ? '' : '<div class="message empty">'+lng.no_options+'</div>';
+				// Блок поиска
+				var html_search = is_search ? '<div class="search"><input type="text" name="custom_select_search"></div>' : '';
 
-        // Создать каркас
-        def_select.after('<div class="select"><div class="header"><div class="title"><div class="cover"></div></div><div class="toggle"></div></div><div class="body">'+html_search+'<div class="options">'+html_options+'</div></div></div>');
+				// Блок поиска
+				var html_options = is_options ? '' : '<div class="message empty">'+lng.no_options+'</div>';
 
-        var
-        custom  = def_select.next('.select'),
-        header  = custom.find('.header'),
-        title   = header.find('.title .cover'),
-        body    = custom.find('.body'),
-        options = body.find('.options'),
-        option  = options.find('.option'),
-        search  = body.find('.search input');
+				// Создать каркас
+				def_select.after('<div class="select"><div class="header"><div class="title"><div class="cover"></div></div><div class="toggle"></div></div><div class="body">'+html_search+'<div class="options">'+html_options+'</div></div></div>');
 
-        // Показать селект
-        custom.animate({ opacity: 1 }, 300);
+				var
+				custom  = def_select.next('.select'),
+				header  = custom.find('.header'),
+				title   = header.find('.title .cover'),
+				body    = custom.find('.body'),
+				options = body.find('.options'),
+				option  = options.find('.option'),
+				search  = body.find('.search input');
 
-        // Вставить все доступные пункты
-        if (def_options.length) {
-          $.each(def_options, function(i, item) {
-            var item = $(item);
-            if (item.text() == '') return;
-            options.append('<div class="option" data-value="'+item.val()+'">'+item.text()+'</div>');
-          });
-        }
+				// Показать селект
+				custom.animate({ opacity: 1 }, 300);
 
-        // Вставить плейсхолдер
-        selected_options();
+				// Вставить все доступные пункты
+				if (def_options.length) {
+					$.each(def_options, function(i, item) {
+						var item = $(item);
+						if (item.text() == '') return;
+						options.append('<div class="option" data-value="'+item.val()+'">'+item.text()+'</div>');
+					});
+				}
 
-        /* События
-        *******************************************************************************
-        */
+				// Вставить плейсхолдер
+				selected_options();
 
-        // При смене значения в дефолтном селекте
-        def_select.on('change', function() {
-          selected_options();
-        });
+				/* События
+				*******************************************************************************
+				*/
 
-        // При клике вне селекта
-        $(window).on('click', function(e) {
-          if (!$(e.target).hasClass('custom')
-          &&  !$(e.target).hasClass('select')
-          &&  !$(e.target).closest('.select').length) {
-            dropdown_close();
-          }
-        });
+				// При смене значения в дефолтном селекте
+				def_select.on('change', function() {
+					selected_options();
+				});
 
-        // При клике по шапке
-        header.on('click', function(e) {
+				// При клике вне селекта
+				$(window).on('click', function(e) {
+					if (!$(e.target).hasClass('custom')
+					&&  !$(e.target).hasClass('select')
+					&&  !$(e.target).closest('.select').length) {
+						dropdown_close();
+					}
+				});
 
-          var target = $(e.target);
+				// При клике по шапке
+				header.on('click', function(e) {
 
-          // При клике по пункту в шапке
-          if (target.hasClass('choice')) {
-            options.find('.option[data-value="'+target.data('value')+'"]').trigger('click');
-          }
+					var target = $(e.target);
 
-          else {
-            toggle_dropdown();
-          }
+					// При клике по пункту в шапке
+					if (target.hasClass('choice')) {
+						options.find('.option[data-value="'+target.data('value')+'"]').trigger('click');
+					}
 
-          return false;
+					else {
+						toggle_dropdown();
+					}
 
-        });
+					return false;
 
-        // При клике по пункту
-        options.find('.option').on('click', function() {
+				});
 
-          var
-          new_item   = $(this),
-          new_title  = new_item.text(),
-          new_value  = new_item.data('value').toString(),
-          is_current = def_select.val().indexOf(new_value) < 0 ? false : true;
+				// При клике по пункту
+				options.find('.option').on('click', function() {
 
-          // Выделить пункт в дефолтном селекте
-          def_select.find('option').filter(function() {
-            return ($(this).text() == new_title) ? true : false;
-          }).prop('selected', (is_current ? false : true));
+					var
+					new_item   = $(this),
+					new_title  = new_item.text(),
+					new_value  = new_item.data('value').toString(),
+					is_current = def_select.val().indexOf(new_value) < 0 ? false : true;
 
-          new_item.toggleClass('selected');
+					// Выделить пункт в дефолтном селекте
+					var def_option = def_select.find('option').filter(function() {
+						return ($(this).text() == new_title) ? true : false;
+					});
+					if (is_current) {
+						def_option.removeAttr('selected');
+					}
+					else {
+						def_option.attr('selected', '');
+					}
 
-          // Единичный выбор (НЕ множественный)
-          if (!is_multiple) {
-            toggle_dropdown();
-          }
+					new_item.toggleClass('selected');
 
-          selected_options();
+					// Единичный выбор (НЕ множественный)
+					if (!is_multiple) {
+						def_option.siblings('option').removeAttr('selected');
+						new_item.siblings('.selected').removeClass('selected');
+						toggle_dropdown();
+					}
 
-        });
+					def_select.trigger('change');
 
-        // Поиск
-        search.on('keyup', function() {
-          var val = search.val();
+				});
 
-          // Искать
-          if (val.length > 2) {
-            var v = 0;
-            options.find('.no_results').remove();
-            options.find('.option').each(function (i, e) {
-              var item = $(e);
-              if (item.text().indexOf(val) == -1) {
-                item.hide(300);
-              }
-              else {
-                item.show(300);
-                v++;
-              }
-            });
-            if (v == 0) {
-              options.append('<div class="message no_results">'+lng.no_results+'</div>')
-            }
-          }
+				// Поиск
+				search.on('keyup', function() {
+					var val = search.val().toLowerCase();
 
-          // Сбросить фильтр
-          else {
-            options.find('.no_results').remove();
-            options.find('.option').show(300);
-          }
+					// Искать
+					if (val.length > 2) {
+						var v = 0;
+						options.find('.no_results').remove();
+						options.find('.option').each(function (i, e) {
+							var item = $(e);
+							if (item.text().toLowerCase().indexOf(val) == -1) {
+								item.hide(300);
+							}
+							else {
+								item.show(300);
+								v++;
+							}
+						});
+						if (v == 0) {
+							options.append('<div class="message no_results">'+lng.no_results+'</div>')
+						}
+					}
 
-        });
+					// Сбросить фильтр
+					else {
+						options.find('.no_results').remove();
+						options.find('.option').show(300);
+					}
 
-        /* Функции
-        *******************************************************************************
-        */
+				});
 
-        function toggle_dropdown() {
-          if (custom.hasClass('opend')) {
-            dropdown_close();
-          }
-          else {
-            dropdown_open();
-          }
-        }
+				// Обработать нажатия на спец клавиши
+				doc.on('keyup', function(e) {
 
-        function dropdown_open() {
-          dropdown_close_all();
-          custom.addClass('opend');
-          body.fadeIn(400, function() {
-          });
-          def_select.blur();
-          body.find('.search input').focus();
-        }
+					// Esc
+					if (e.keyCode == 27) {
+						dropdown_close();
+					}
 
-        function dropdown_close() {
-          body.fadeOut(400, function() {
-            custom.removeClass('opend');
-          });
-          body.find('.search input').val('').blur();
-        }
+					// Стрелка вверх
+					else if (e.keyCode == 38) {
+					}
 
-        function dropdown_close_all() {
-          var select_opend = $('.select.opend');
-          select_opend.removeClass('opend');
-          select_opend.find('.body').fadeOut();
-          select_opend.find('.body .search input').val('').blur();
-        }
+					// Стрелка вниз
+					else if (e.keyCode == 40) {
+					}
 
-        function selected_options() {
-          title.html('');
-          var selected = def_options.filter(':selected');
-          if (selected.length) {
-            selected.each(function(i, e) {
-              var item = $(e);
-              title.append('<span class="choice" data-value="'+item.val()+'">'+item.text()+'</span>');
-            });
-          }
-          else {
-            title.append('<span class="placeholder">'+def_select.attr('placeholder')+'</span>');
-          }
-        }
+					// Enter
+					else if (e.keyCode == 13) {
+					}
 
-      });
-    }
+				});
 
-    if (action == 'reinit') {
-      return this.each(function(i, e) {
-        var select = $(e),
-            custom = select.next('.select');
-        if (custom.length) {
-          custom.animate({ opacity: 0 }, 300, function(){
-            custom.remove();
-            select.customSelect();
-          });
-        }
-      });
-    }
+				/* Функции
+				*******************************************************************************
+				*/
 
-  }
+				function toggle_dropdown() {
+					if (custom.hasClass('opend')) {
+						dropdown_close();
+					}
+					else {
+						dropdown_open();
+					}
+				}
+
+				function dropdown_open() {
+					dropdown_close_all();
+					custom.addClass('opend');
+					body.fadeIn(400, function() {
+					});
+					def_select.blur();
+					body.find('.search input').focus();
+				}
+
+				function dropdown_close() {
+					body.fadeOut(400, function() {
+						custom.removeClass('opend');
+					});
+					body.find('.search input').val('').blur();
+					options.find('.option').show();
+				}
+
+				function dropdown_close_all() {
+					var select_opend = $('.select.opend');
+					select_opend.removeClass('opend');
+					select_opend.find('.body').fadeOut();
+					select_opend.find('.body .search input').val('').blur();
+				}
+
+				function selected_options() {
+					title.html('');
+					var selected = def_options.filter('[selected]');
+					console.log(selected);
+					if (selected.length) {
+						selected.each(function(i, e) {
+							var item = $(e);
+							title.append('<span class="choice" data-value="'+item.val()+'">'+item.text()+'</span>');
+						});
+					}
+					else if (def_select.attr('placeholder')) {
+						title.append('<span class="placeholder">'+def_select.attr('placeholder')+'</span>');
+					}
+				}
+
+			});
+		}
+
+		if (action == 'reinit') {
+			return this.each(function(i, e) {
+				var select = $(e),
+						custom = select.next('.select');
+				if (custom.length) {
+					custom.animate({ opacity: 0 }, 300, function(){
+						custom.remove();
+						select.customSelect();
+					});
+				}
+			});
+		}
+
+	}
 
 })(jQuery);
